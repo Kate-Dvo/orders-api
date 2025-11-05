d6fc28ce0d62ffbddf104e052431abfdff94439e
Kate-Dvo <orev184@gmail.com>
2025-11-05 16:00:56 +0200

    Added Db context to Program

--- src/OrdersApi.Api/Program.cs
+++ src/OrdersApi.Api/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.EntityFrameworkCore;
 using OrdersApi.Domain.Configuration;
+using OrdersApi.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +15,10 @@ builder.Services.AddSwaggerGen();
 builder.Services.Configure<JwtSettings>(
     builder.Configuration.GetSection("JwtSettings"));
 
-//TODO: Add DbContext
+//Add DbContext
+builder.Services.AddDbContext<OrdersDbContext>(options => 
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 //TODO: Add Authentication
 //TODO: Add other services 
 
@@ -24,6 +29,9 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    var address = app.Services.GetRequiredService<IServer>()
+        .Features.Get<IServerAddressesFeature>()?.Addresses;
+    Console.WriteLine($"Swagger UI: {address?.FirstOrDefault()}/swagger");
 }
 
 app.UseHttpsRedirection();
