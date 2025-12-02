68b600bcf755f93adf39276e23b0c581f18a8b93
Kate-Dvo <orev184@gmail.com>
2025-12-02 23:00:46 +0200

    Create Consts.cs

---
+++ src/OrdersApi.Api/Helpers/Consts.cs
@@ -0,0 +1,8 @@
+namespace OrdersApi.Api.Helpers;
+
+public static class Consts
+{
+    public const string FixedRateLimit = "fixed";
+    public const string SlidingRateLimit = "sliding";
+    public const string PerUserTokenBucket = "per-user";
+}
